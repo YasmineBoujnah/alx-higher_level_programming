3f555ba420eaff13a5bf989d390258295ecc74ee
Yasmine Boujnah <132377962+YasmineBoujnah@users.noreply.github.com>
2023-11-03 23:46:06 +0100

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+    #!/usr/bin/python3
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
     if a < b:
         c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
+        for n in range(4, 6):
+            c = add(c, n)
         return c
     else:
         return sub(a, b)
