7840261796f4f2fd86caf3ea773c35a20cb701b0
Yasmine Boujnah <132377962+YasmineBoujnah@users.noreply.github.com>
2023-11-03 23:36:01 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for word in dir(hidden_4):
-        if not(word[0] == '_' and word[1] == '_'):
-            print("{}".format(word))
+
+    for i in dir(hidden_4):
+        if i[:2] == "__":
+            continue
+        print(i)
