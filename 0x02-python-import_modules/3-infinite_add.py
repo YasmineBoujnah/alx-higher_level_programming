08ab05d3f3d09e922b943f327d4c66804b1efaae
Yasmine Boujnah <132377962+YasmineBoujnah@users.noreply.github.com>
2023-11-03 23:37:48 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    length = len(argv) - 1
-    sum1 = 0
-    i = 1
-    if (length == 0):
-        print("{}".format(sum1))
-    else:
-        while i <= length:
-            sum1 += int(argv[i])
-            i += 1
-        print("{}".format(sum1))
+    sum = 0
+    for a in range(1, len(argv)):
+        sum += int(argv[a])
+    print(sum)
